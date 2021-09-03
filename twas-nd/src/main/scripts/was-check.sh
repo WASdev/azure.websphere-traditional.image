dab40af306299479299535e24dcc65e8c127b2d5
Jianguo Ma <jiangma@microsoft.com>
2021-09-03 13:45:14 +0800

    move hard-coded values to properties file
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- twas-nd/src/main/scripts/was-check.sh
+++ twas-nd/src/main/scripts/was-check.sh
@@ -17,15 +17,14 @@
 # Get tWAS installation properties
 source /datadrive/virtualimage.properties
 
-wasLog=/var/log/cloud-init-was.log
-echo "$(date): Start to check entitlement." > $wasLog
+echo "$(date): Start to check entitlement." > $WAS_LOG_PATH
 
 # Read custom data from ovf-env.xml
 customData=`xmllint --xpath "//*[local-name()='Environment']/*[local-name()='ProvisioningSection']/*[local-name()='LinuxProvisioningConfigurationSet']/*[local-name()='CustomData']/text()" /var/lib/waagent/ovf-env.xml`
 read -r -a ibmIdCredentials <<< "$(echo $customData | base64 -d)"
 
 # Check whether IBMid is entitled or not
-result=Unentitled
+result=$UNENTITLED
 if [ ${#ibmIdCredentials[@]} -eq 2 ]; then
     userName=${ibmIdCredentials[0]}
     password=${ibmIdCredentials[1]}
@@ -33,30 +32,30 @@ if [ ${#ibmIdCredentials[@]} -eq 2 ]; then
     ${IM_INSTALL_DIRECTORY}/eclipse/tools/imutilsc saveCredential -secureStorageFile storage_file \
         -userName "$userName" -userPassword "$password" -passportAdvantage
     if [ $? -ne 0 ]; then
-        echo "Cannot connect to Passport Advantage while saving the credential to the secure storage file." >> $wasLog
+        echo "Cannot connect to Passport Advantage while saving the credential to the secure storage file." >> $WAS_LOG_PATH
     fi
     
     output=$(${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl listAvailablePackages -cPA -secureStorageFile storage_file)
-    echo $output | grep -q "$WAS_ND_VERSION_ENTITLED" && result=Entitled
-    echo $output | grep -q "$NO_PACKAGES_FOUND" && result=Undefined
+    echo $output | grep -q "$WAS_ND_VERSION_ENTITLED" && result=$ENTITLED
+    echo $output | grep -q "$NO_PACKAGES_FOUND" && result=$UNDEFINED
 else
-    echo "Invalid input format." >> $wasLog
+    echo "Invalid input format." >> $WAS_LOG_PATH
 fi
 
-echo "$(date): Entitlement check completed, start to update WebSphere installation." >> $wasLog
-if [ ${result} = Entitled ]; then
+echo "$(date): Entitlement check completed, start to update WebSphere installation." >> $WAS_LOG_PATH
+if [ ${result} = $ENTITLED ]; then
     # Update all packages for the entitled user
     output=$(${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl updateAll -repositories "$REPOSITORY_URL" \
         -acceptLicense -log log_file -installFixes recommended -secureStorageFile storage_file -preferences $SSL_PREF,$DOWNLOAD_PREF -showProgress)
-    echo "$output" >> $wasLog
+    echo "$output" >> $WAS_LOG_PATH
 else
     # Remove tWAS installation for the un-entitled or undefined user
     output=$(${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl uninstall "$WAS_ND_TRADITIONAL" "$IBM_JAVA_SDK" -installationDirectory ${WAS_ND_INSTALL_DIRECTORY})
-    echo "$output" >> $wasLog
+    echo "$output" >> $WAS_LOG_PATH
     rm -rf /datadrive/IBM && rm -rf /datadrive/virtualimage.properties
 fi
-echo "$(date): WebSphere installation updated." >> $wasLog
-echo ${result} >> $wasLog
+echo "$(date): WebSphere installation updated." >> $WAS_LOG_PATH
+echo ${result} >> $WAS_LOG_PATH
 
 # Scrub the custom data from files which contain sensitive information
 if grep -q "CustomData" /var/lib/waagent/ovf-env.xml; then
