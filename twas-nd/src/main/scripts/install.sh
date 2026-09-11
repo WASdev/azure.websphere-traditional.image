1b9214c9a067fc639b505944946d68becbf4fc8b
Kevin Ortega <ortegake@us.ibm.com>
2026-09-11 08:17:34 -0400

    Try different approach

--- twas-nd/src/main/scripts/install.sh
+++ twas-nd/src/main/scripts/install.sh
@@ -62,65 +62,54 @@ unzip -q "$IM_INSTALL_KIT" -d im_installer
 chmod -R 755 ./im_installer/*
 ./im_installer/userinstc -log log_file -acceptLicense -installationDirectory ${IM_INSTALL_DIRECTORY}
 
-# Create master password file required for headless secure storage on IM 1.9+
-echo "IMmasterPassword" > master_password_file
-
-# Save credentials to a secure storage file
-${IM_INSTALL_DIRECTORY}/eclipse/tools/imutilsc saveCredential -secureStorageFile storage_file \
-    -masterPasswordFile master_password_file \
-    -userName "$userName" -userPassword "$password" -url "$REPOSITORY_URL"
-
 # Check whether IBMid is entitled or not
-if [ $? -eq 0 ]; then
-    output=$(${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl listAvailablePackages \
-        -repositories "$REPOSITORY_URL" -secureStorageFile storage_file \
-        -masterPasswordFile master_password_file)
-    if echo "$output" | grep "$WAS_ND_VERSION_ENTITLED"; then
-        echo "$(date): IBMid entitlement check succeeded."
-    elif echo "$output" | grep "$NO_PACKAGES_FOUND"; then
-        echo "$(date): IBMid entitlement check is not available."
-        rm -rf storage_file && rm -rf log_file && rm -rf master_password_file
-        exit 1
-    else
-        echo "$(date): IBMid entitlement check failed."
-        rm -rf storage_file && rm -rf log_file && rm -rf master_password_file
-        exit 1
-    fi
+output=$(${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl listAvailablePackages \
+    -repositories "$REPOSITORY_URL" \
+    -credential "$REPOSITORY_URL,$userName,$password")
+if echo "$output" | grep "$WAS_ND_VERSION_ENTITLED"; then
+    echo "$(date): IBMid entitlement check succeeded."
+elif echo "$output" | grep "$NO_PACKAGES_FOUND"; then
+    echo "$(date): IBMid entitlement check is not available."
+    rm -rf log_file
+    exit 1
 else
-    echo "$(date): Cannot connect to Passport Advantage."
-    rm -rf storage_file && rm -rf log_file && rm -rf master_password_file
+    echo "$(date): IBMid entitlement check failed."
+    rm -rf log_file
     exit 1
 fi
 
 # Install IBM WebSphere Application Server Network Deployment V9 using IBM Instalation Manager
-${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl install "$WAS_ND_TRADITIONAL" "$IBM_JAVA_SDK" -repositories "$REPOSITORY_URL" \
+${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl install "$WAS_ND_TRADITIONAL" "$IBM_JAVA_SDK" \
+    -repositories "$REPOSITORY_URL" \
+    -credential "$REPOSITORY_URL,$userName,$password" \
     -installationDirectory ${WAS_ND_INSTALL_DIRECTORY}/ -sharedResourcesDirectory ${IM_SHARED_DIRECTORY}/ \
-    -secureStorageFile storage_file -masterPasswordFile master_password_file \
     -acceptLicense -preferences $SSL_PREF,$DOWNLOAD_PREF -showProgress -log log_file
 
 if [ $? -eq 0 ]; then
     echo "$(date): IBM WebSphere Application Server Network Deployment V9 installed successfully."
 else
     echo "$(date): IBM WebSphere Application Server Network Deployment V9 failed to be installed."
-    rm -rf storage_file && rm -rf log_file && rm -rf master_password_file
+    rm -rf log_file
     exit 1
 fi
 
 # Update packages and apply iFixes
-${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl updateAll -repositories "$REPOSITORY_URL" \
-    -acceptLicense -log log_file -installFixes recommended -secureStorageFile storage_file \
-    -masterPasswordFile master_password_file -preferences $SSL_PREF,$DOWNLOAD_PREF -showProgress
+${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl updateAll \
+    -repositories "$REPOSITORY_URL" \
+    -credential "$REPOSITORY_URL,$userName,$password" \
+    -acceptLicense -log log_file -installFixes recommended \
+    -preferences $SSL_PREF,$DOWNLOAD_PREF -showProgress
 
 if [ $? -eq 0 ]; then
     echo "$(date): Successfully updated packages and applied iFixes."
 else
     echo "$(date): Failed to update packages and apply iFixes."
-    rm -rf storage_file && rm -rf log_file && rm -rf master_password_file
+    rm -rf log_file
     exit 1
 fi
 
 # Remove temporary files
-rm -rf storage_file && rm -rf log_file && rm -rf master_password_file
+rm -rf log_file
 
 # Install other packages
 yum install cifs-utils -y -q
