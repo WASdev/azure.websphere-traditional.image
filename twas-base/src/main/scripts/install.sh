968b2ce092c4ebb5caa731a0592e169cf1dfd40f
Kevin Ortega <ortegake@us.ibm.com>
2026-09-11 12:22:40 -0400

    fix twas-base

--- twas-base/src/main/scripts/install.sh
+++ twas-base/src/main/scripts/install.sh
@@ -93,7 +93,7 @@ else
 fi
 
 # Install IBM WebSphere Application Server V9 using IBM Instalation Manager
-${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl install "$WAS_BASE_TRADITIONAL" "$IBM_JAVA_SDK" \
+${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl install "$WAS_BASE_TRADITIONAL" \
     -repositories "$BASE_REPOSITORY_URL,$REPOSITORY_URL" \
     -secureStorageFile storage_file -masterPasswordFile master_password_file \
     -installationDirectory ${WAS_BASE_INSTALL_DIRECTORY}/ -sharedResourcesDirectory ${IM_SHARED_DIRECTORY}/ \
